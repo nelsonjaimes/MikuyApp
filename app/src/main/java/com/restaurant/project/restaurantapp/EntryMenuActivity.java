87669476443ab1235e99cde2865a2ebf679ef034
jNelson <nelsonjaimesgonzales@gmail.com>
2018-05-03 16:38:09 -0500

    RestaurantApp: Trabajo 3-05-2018 II

--- app/src/main/java/com/restaurant/project/restaurantapp/EntryMenuActivity.java
+++ app/src/main/java/com/restaurant/project/restaurantapp/EntryMenuActivity.java
@@ -1,13 +1,25 @@
 package com.restaurant.project.restaurantapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class EntryMenuActivity extends AppCompatActivity {
+    private TextView tvSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_entry);
+        tvSignIn = findViewById(R.id.tvSignIn);
+        tvSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EntryMenuActivity.this
+                        , SignInActivity.class));
+            }
+        });
     }
 }
