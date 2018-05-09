ac508cce4c9e3c03dd1f0e23f832f106e258511d
jNelson <nelsonjaimesgonzales@gmail.com>
2018-05-09 15:19:39 -0500

    MikuyApp 9-05-2018 II

--- app/src/main/java/com/restaurant/project/restaurantapp/reservation/MyReservationsFragment.java
+++ app/src/main/java/com/restaurant/project/restaurantapp/reservation/MyReservationsFragment.java
@@ -22,6 +22,7 @@ public class MyReservationsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_my_reservations, container, false);
     }
 
